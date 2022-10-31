9293ff364f122a22173555a2ef78a1a460d295f9
wukongO-O <l.shenglan@gmail.com>
2022-10-30 23:09:17 -0400

    build BST function

--- bst.js
+++ bst.js
@@ -1,48 +1,50 @@
-const Node = (rootNode, leftChild, rightChild) => {
-    let node = {};
-    node.rootNode = rootNode;
-    node.leftChild = leftChild;
-    node.rightChild = rightChild;
-    return node;
-};
-const Tree = (arr) => {
-    let tree = {arr};
-    let root;
-    
-    const buildTree = (arr) => {
-        const sortedArr = arr.sort();
-        let newSortedArr = [];
-        for (let i = 0; i < sortedArr.length; i++) {
-            if (sortedArr[i] == sortedArr[i+1]) {
-                return;
-            } else {
-                newSortedArr.push(sortedArr[i]);
-            }
-        }
-        const midIndex = Math.round(newSortedArr.length/2);
-        const leftArr = newSortedArr.slice(0, midIndex);
-        const rightArr = newSortedArr.slice(midIndex, newSortedArr.length);
-        buildTree(leftArr);
-        buildTree(rightArr);
-        root = newSortedArr[midIndex]
-        return root;
+//Build a node factory
+function Node(rootNode, leftChild = null, rightChild = null) {
+    return {
+        rootNode,
+        leftChild,
+        rightChild
     };
-    //visualize bst
-    const prettyPrint = (node, prefix = '', isLeft = true) => {
-        node = root;
-        if (node.right !== null) {
-          prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-        }
-        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-        if (node.left !== null) {
-          prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-        }
-      }
+}
+//Build a tree factory taking an array & returning level-0 root
+function Tree(arr) {
+    return {
+        root0: buildTree(arr)
+    };
+}
+//function turing an array into a BST
+function buildTree (arr) {
+    const preppedArr = removeDups(sortArr(arr));
+    return buildTreeFromPreppedArr(preppedArr, 0, preppedArr.length - 1);
+}
 
-    return tree;
-};
+function sortArr(arr) {
+    return arr.sort((a,b) => a - b);
+}
+function removeDups(arr) {
+    let uniqueArr = [];
+    arr.forEach(item => {
+        if (!uniqueArr.includes(item)) {
+            uniqueArr.push(item);
+        }
+    })
+    return uniqueArr;
+}
+function buildTreeFromPreppedArr(sortedArr, firstInd, lastInd) {
+    if (firstInd > lastInd) return null;
+    let midIndex = parseInt((firstInd + lastInd)/2);
+    let currentRoot = Node(sortedArr[midIndex]);
+    currentRoot.leftChild = buildTreeFromPreppedArr(sortedArr, firstInd, midIndex - 1);
+    currentRoot.rightChild = buildTreeFromPreppedArr(sortedArr, midIndex + 1, lastInd);
+    return currentRoot;
+}
+
 
 const test1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+console.log(sortArr(test1));
+console.log(removeDups(test1));
 const tree1 = Tree(test1); 
-//console.log(tree1.buildTree(tree1));
-tree1.prettyPrint();
+console.log(tree1);
+
+
+
