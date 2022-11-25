d14c6a66f0a43110aed55659eb3ced385688bb7f
wukongO-O <l.shenglan@gmail.com>
2022-11-24 21:50:12 -0500

    dfs function via recursion for directed graph

--- graph.js
+++ graph.js
@@ -1,20 +1,29 @@
 //DFS directed graph via iteration
 const depthFirstI = (graphAdjList, nodeKey) => {
-    const stack = [nodeKey];
+    const stack = [nodeKey]; //a stack to store nodes
     const result = [];
 
     while (stack.length > 0) {
-        const currentNode = stack.pop();
+        const currentNode = stack.pop(); //LIFO
         result.push(currentNode);
         for (let neighbor of graphAdjList[currentNode]) {
             stack.push(neighbor);
-        }
+        };
     };
+
     return result;
+};
 
-}
-//DFS graph recursively
+//DFS directed graph recursively
+const depthFirstRec = (graphAdjList, nodeKey, result = []) => {
+    result.push(nodeKey);
 
+    for (let neighbor of graphAdjList[nodeKey]) {
+        depthFirstRec(graphAdjList, neighbor, result);
+    };
+    
+    return result;
+};
 //BFS graph
 
 //testing
@@ -26,5 +35,7 @@ const graph = {
     e: [],
     f: []
 }
-console.log(depthFirstI(graph, 'b')); //[ 'b', 'd', 'f' ]
-console.log(depthFirstI(graph, 'a')); // [ 'a', 'c', 'e', 'b', 'd', 'f' ]
+// console.log(depthFirstI(graph, 'b')); //[ 'b', 'd', 'f' ]
+// console.log(depthFirstI(graph, 'a')); // [ 'a', 'c', 'e', 'b', 'd', 'f' ]
+console.log(depthFirstRec(graph, 'a'));  //[ 'a', 'b', 'd', 'f', 'c', 'e' ]
+console.log(depthFirstRec(graph, 'b'));  //[ 'b', 'd', 'f' ]
