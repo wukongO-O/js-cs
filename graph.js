9d02530aceb4d966be680368436dd91d21c6d0e7
wukongO-O <l.shenglan@gmail.com>
2022-11-24 23:56:25 -0500

    check if a path between 2 nodes exists via iteration & recursion

--- graph.js
+++ graph.js
@@ -24,7 +24,8 @@ const depthFirstRec = (graphAdjList, nodeKey, result = []) => {
 
     return result;
 };
-//BFS graph
+
+//BFS directed graph
 const breadthFirst = (graphAdjList, nodeKey) => {
     const queue = [nodeKey]; //a queue initiated with starting node's key to store nodes
     let result = [];
@@ -39,6 +40,34 @@ const breadthFirst = (graphAdjList, nodeKey) => {
 
     return result;
 };
+
+// function to check if a path between node a & b exists in a directed graph: iteration / recursion
+const hasPathI = (graphAdjList, nodeA, nodeB) => {
+    const queue = [nodeA]; //use bfs
+
+    while (queue.length > 0) {
+        let currentNode = queue.shift();
+        if (currentNode === nodeB) return true;
+        for (let neighbor of graphAdjList[currentNode]) {
+            queue.push(neighbor);
+        }
+    };
+
+    return false;
+}
+const hasPahtRec = (graphAdjList, nodeA, nodeB) => {
+    if (nodeA === nodeB) return true;
+
+    for (let neighbor of graphAdjList[nodeA]) {
+        if (hasPahtRec(graphAdjList, neighbor, nodeB) === true) return true;
+    };
+    
+    return false;
+};
+
+//undirected graph 
+
+
 //testing
 const graph1 = {
     a: ['b', 'c'],
@@ -52,4 +81,7 @@ const graph1 = {
 // console.log(depthFirstI(graph1, 'a')); // [ 'a', 'c', 'e', 'b', 'd', 'f' ]
 // console.log(depthFirstRec(graph1, 'a'));  //[ 'a', 'b', 'd', 'f', 'c', 'e' ]
 // console.log(depthFirstRec(graph1, 'b'));  //[ 'b', 'd', 'f' ]
-console.log(breadthFirst(graph1, 'a'));  // [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+// console.log(breadthFirst(graph1, 'a'));  // [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+console.log(hasPathI(graph1, 'c', 'f'));  // false
+console.log(hasPahtRec(graph1, 'c', 'f'));  // false
+console.log(hasPahtRec(graph1, 'a', 'f'));  // true
